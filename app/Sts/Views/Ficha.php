17c1f5bc7a9050c0ca7af0913275053c333ff9c3
Jhonny A. Soares <74833753+JhonnyASoares@users.noreply.github.com>
2025-01-05 18:58:43 -0300

    ajustado um pouco do desing da ficha

--- app/Sts/Views/Ficha.php
+++ app/Sts/Views/Ficha.php
@@ -1,25 +1,66 @@
 <div class="container">
     <section>
         <h1>Ficha Page</h1>
-        <div class="ficha-char">
-            <div>
-                <div class="ficha-img">
+        <div class="ficha-char flex-collumn">
+            <div class="char-data flex-row">
+                <div class="char-img">
                     <img src="/app/Sts/Assets/Images/Backgrounds/night_background.jpg" alt="">
                 </div>
-                <div class="ficha-info"> 
-                    <div>
+                <div class="char-info flex-collumn"> 
+                    <div class="flex-collumn">
                         <label for="name">Name</label>
                         <input type="text" id="name" name="name" value="Pikachu">
                     </div>
-                    <div>
+                    <div class="flex-collumn">
                         <label for="type">Type/s</label>
                         <input type="text" id="type" name="type" value="Electric">
                     </div>
-                    <div>
+                    <div class="flex-collumn">
                         <label for="class">Class</label>
-                        <input type="text" id="class" name="class" value="100">
+                        <input type="text" id="class" name="class" value="Professor">
                     </div>
                 </div>
+            </div>
+            <div class="char-finance flex-row">
+                <div class="flex-collumn">
+                    <label for="money">Money</label>
+                    <input type="number" id="money" name="money" value="1000">
+                </div>
+                <button class="medals"><img src="/app/Sts/Assets/Images/Gifs/pikachu.gif"></button>
+                <div class="flex-collumn">
+                    <label for="bp">BP</label>
+                    <input type="number" id="bp" name="bp" value="100">
+                </div>
+            </div>
+            <div class="char-inventory flex-column">
+                <label>Inventory</label>
+                <div class="char-itens">
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                    <div class="item">
+                        <input type="text" name="name" value="Oran Berry">
+                        <input type="number" name="qtd" value="1">
+                    </div>
+                </div>
+                <button>+</button>
             </div>
         </div>
     </section>
